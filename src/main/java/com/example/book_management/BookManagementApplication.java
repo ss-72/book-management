923839e292f156413260795b9f629a8bce69a447
ss-72 <te230126@gmail.com>
2026-05-15 14:38:02 +0900

    演習10MySQL

--- src/main/java/com/example/book_management/BookManagementApplication.java
+++ src/main/java/com/example/book_management/BookManagementApplication.java
@@ -3,13 +3,13 @@ package com.example.book_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-// ★追加する機能のインポート
+
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example")
-// ★データベース用のスキャン設定を2行追加します
+
 @EnableJpaRepositories(basePackages = "com.example.repository")
 @EntityScan(basePackages = "com.example.model")
 public class BookManagementApplication {
