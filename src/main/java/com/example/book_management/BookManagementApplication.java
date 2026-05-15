32b6500b3c3c9ed5b7034b85b2ac09357d06cfdb
ss-72 <te230126@gmail.com>
2026-05-15 12:06:34 +0900

    Spring Data演習

--- src/main/java/com/example/book_management/BookManagementApplication.java
+++ src/main/java/com/example/book_management/BookManagementApplication.java
@@ -3,10 +3,15 @@ package com.example.book_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+// ★追加する機能のインポート
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-// ★以下の1行を追加して、com.example全体を読み込ませます
 @ComponentScan(basePackages = "com.example")
+// ★データベース用のスキャン設定を2行追加します
+@EnableJpaRepositories(basePackages = "com.example.repository")
+@EntityScan(basePackages = "com.example.model")
 public class BookManagementApplication {
 
     public static void main(String[] args) {
